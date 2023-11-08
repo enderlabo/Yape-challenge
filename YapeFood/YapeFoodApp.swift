25bf897d8c9ce01becdd4f2885f59c7060a6ffdf
Elderson Laborit <enderlabo@gmail.com>
2023-11-08 15:57:49 -0500

    Initial Commit

---
+++ YapeFood/YapeFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  YapeFoodApp.swift
+//  YapeFood
+//
+//  Created by Elderson Laborit on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct YapeFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
